b1fb73121e5bfc173b0a710ec87ccf08682b7a14
Omar Khallaf <ok7831495@gmail.com>
2024-04-14 02:51:30 +0200

    initial sln for Picture validate click

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblName = new System.Windows.Forms.Label();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblName.Location = new System.Drawing.Point(724, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(398, 51);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Tic-Tac-Toe Game";
+            // 
+            // pb3
+            // 
+            this.pb3.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pb3.Location = new System.Drawing.Point(1109, 107);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(162, 103);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb3.TabIndex = 1;
+            this.pb3.TabStop = false;
+            this.pb3.Tag = "Question";
+            this.pb3.Click += new System.EventHandler(this.pb3_Click);
+            // 
+            // pb4
+            // 
+            this.pb4.Image = ((System.Drawing.Image)(resources.GetObject("pb4.Image")));
+            this.pb4.Location = new System.Drawing.Point(605, 316);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(162, 103);
+            this.pb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb4.TabIndex = 2;
+            this.pb4.TabStop = false;
+            this.pb4.Tag = "Question";
+            this.pb4.Click += new System.EventHandler(this.pb4_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.Image = ((System.Drawing.Image)(resources.GetObject("pb1.Image")));
+            this.pb1.Location = new System.Drawing.Point(605, 107);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(162, 103);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb1.TabIndex = 3;
+            this.pb1.TabStop = false;
+            this.pb1.Tag = "Question";
+            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb8
+            // 
+            this.pb8.Image = ((System.Drawing.Image)(resources.GetObject("pb8.Image")));
+            this.pb8.Location = new System.Drawing.Point(873, 524);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(162, 103);
+            this.pb8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb8.TabIndex = 4;
+            this.pb8.TabStop = false;
+            this.pb8.Tag = "Question";
+            this.pb8.Click += new System.EventHandler(this.pb8_Click);
+            // 
+            // pb5
+            // 
+            this.pb5.Image = ((System.Drawing.Image)(resources.GetObject("pb5.Image")));
+            this.pb5.Location = new System.Drawing.Point(873, 316);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(162, 103);
+            this.pb5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb5.TabIndex = 5;
+            this.pb5.TabStop = false;
+            this.pb5.Tag = "Question";
+            this.pb5.Click += new System.EventHandler(this.pb5_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.Image = ((System.Drawing.Image)(resources.GetObject("pb2.Image")));
+            this.pb2.Location = new System.Drawing.Point(860, 107);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(162, 103);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb2.TabIndex = 6;
+            this.pb2.TabStop = false;
+            this.pb2.Tag = "Question";
+            this.pb2.Click += new System.EventHandler(this.pb2_Click);
+            // 
+            // pb9
+            // 
+            this.pb9.Image = ((System.Drawing.Image)(resources.GetObject("pb9.Image")));
+            this.pb9.Location = new System.Drawing.Point(1109, 524);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(162, 103);
+            this.pb9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb9.TabIndex = 7;
+            this.pb9.TabStop = false;
+            this.pb9.Tag = "Question";
+            this.pb9.Click += new System.EventHandler(this.pb9_Click);
+            // 
+            // pb6
+            // 
+            this.pb6.Image = ((System.Drawing.Image)(resources.GetObject("pb6.Image")));
+            this.pb6.Location = new System.Drawing.Point(1109, 316);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(162, 103);
+            this.pb6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb6.TabIndex = 8;
+            this.pb6.TabStop = false;
+            this.pb6.Tag = "Question";
+            this.pb6.Click += new System.EventHandler(this.pb6_Click);
+            // 
+            // pb7
+            // 
+            this.pb7.Image = ((System.Drawing.Image)(resources.GetObject("pb7.Image")));
+            this.pb7.Location = new System.Drawing.Point(605, 524);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(162, 103);
+            this.pb7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb7.TabIndex = 9;
+            this.pb7.TabStop = false;
+            this.pb7.Tag = "Question";
+            this.pb7.Click += new System.EventHandler(this.pb7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1377, 748);
+            this.Controls.Add(this.pb7);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb9);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb8);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.lblName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb7;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,187 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tic_Tac_Toe_Game.Properties;
 
 namespace Tic_Tac_Toe_Game
 {
     public partial class Form1 : Form
     {
+      
+
+        string[] imagePaths = {
+    @"G:\Images\O.png",
+    @"G:\Images\X.png"
+};
+        
+       
         public Form1()
         {
             InitializeComponent();
+
+           
+        }
+
+        static int GenerateRandomNumber(int min=0, int max=1)
+        {
+            Random random = new Random();
+            return random.Next(min, max + 1); // The "+1" ensures that the maximum value is inclusive
+        }
+
+        private string LoadRandomPathImage()
+        {
+
+            return imagePaths[GenerateRandomNumber()];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Color whi = Color.White;
+
+            Pen pen = new Pen(whi);
+            pen.Width = 10;
+
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            //Draw Vertical Lines
+            e.Graphics.DrawLine(pen, 450, 380, 950, 380);
+            e.Graphics.DrawLine(pen, 450, 200, 950, 200);
+
+            //Draw Horizontial Lines
+            e.Graphics.DrawLine(pen,610, 50, 610, 540);
+            e.Graphics.DrawLine(pen, 800, 50, 800, 540);
+        }
+
+        private void pb4_Click(object sender, EventArgs e)
+        {
+            if (pb4.Tag == "Question")
+            {
+                pb4.Image = Image.FromFile(LoadRandomPathImage());
+                pb4.Tag = "Changed";
+            }
+            else
+            {
+                MessageBox.Show("Can't change");
+            }
+        }
+
+        private void pb3_Click(object sender, EventArgs e)
+        {
+            if (pb3.Tag == "Question")
+            {
+                pb3.Image = Image.FromFile(LoadRandomPathImage());
+                pb3.Tag = "Changed";
+            }
+            else
+            {
+                MessageBox.Show("Can't change");
+            }
+        }
+
+        private void pb1_Click(object sender, EventArgs e)
+        {
+          
+            if(pb1.Tag == "Question")
+            {
+                pb1.Tag = "Changed";
+                pb1.Image = Image.FromFile(LoadRandomPathImage());
+               
+            }
+            else
+            {
+                MessageBox.Show("Can't change");
+            }
+               
+        }
+
+        private void pb2_Click(object sender, EventArgs e)
+        {
+            if (pb2.Tag == "Question")
+            {
+                pb2.Tag = "Changed";
+                pb2.Image = Image.FromFile(LoadRandomPathImage());
+                
+            }
+            else
+            {
+                MessageBox.Show("Can't change");
+            }
+        }
+
+        private void pb5_Click(object sender, EventArgs e)
+        {
+            if (pb5.Tag == "Question")
+            {
+                pb5.Tag = "Changed";
+                pb5.Image = Image.FromFile(LoadRandomPathImage());
+            }
+            else
+            {
+                MessageBox.Show("Can't change");
+            }
+        }
+
+        private void pb6_Click(object sender, EventArgs e)
+        {
+            if (pb6.Tag == "Question")
+            {
+                pb6.Tag = "Changed";
+                pb6.Image = Image.FromFile(LoadRandomPathImage());
+            }
+            else
+            {
+                MessageBox.Show("Can't change");
+            }
+        }
+
+        private void pb9_Click(object sender, EventArgs e)
+        {
+            if (pb9.Tag == "Question")
+            {
+                pb9.Tag = "Changed";
+                pb9.Image = Image.FromFile(LoadRandomPathImage());
+            }
+            else
+            {
+                MessageBox.Show("Can't change");
+            }
+        }
+
+        private void pb8_Click(object sender, EventArgs e)
+        {
+
+            if (pb8.Tag == "Question")
+            {
+                pb8.Tag = "Changed";
+                pb8.Image = Image.FromFile(LoadRandomPathImage());
+            }
+            else
+            {
+                MessageBox.Show("Can't change");
+            }
+        }
+
+        private void pb7_Click(object sender, EventArgs e)
+        {
+            if (pb7.Tag == "Question")
+            {
+                pb7.Tag = "Changed";
+                pb7.Image = Image.FromFile(LoadRandomPathImage());
+            }
+            else
+            {
+                MessageBox.Show("Can't change");
+            }
+
         }
     }
 }
